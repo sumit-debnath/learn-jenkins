88ed86c2f12425340856f9c3ce33b87e6c7bd56f
sumit <sdnatheh@gmail.com>
2023-07-04 15:43:19 +0530

    modified

--- shared-library/src/com/example/WorkspaceCleanup.groovy
+++ shared-library/src/com/example/WorkspaceCleanup.groovy
@@ -1,10 +1,20 @@
-package com.example
+package org.example
 
-class WorkspaceCleanup {
-    static void cleanup() {
-        def ws = Jenkins.instance.getWorkspace(currentBuild.rawBuild.getProject())
-        ws.deleteRecursive()
-        ws.mkdirs()
-        echo "Workspace cleaned up."
-    }
+def call() {
+  // Get the current build
+  def currentBuild = Jenkins.instance.getItemByFullName(env.JOB_NAME).getBuildByNumber(env.BUILD_NUMBER)
+
+  // Get the workspace path
+  def workspace = currentBuild.getWorkspace().getRemote()
+
+  // Clean up the workspace
+  cleanWorkspace(workspace)
+}
+
+def cleanWorkspace(String workspace) {
+  // Delete all files and directories in the workspace
+  sh "rm -rf ${workspace}/*"
+
+  // Optional: Delete hidden files and directories in the workspace
+  // sh "rm -rf ${workspace}/.*"
 }
--- shared-library/vars/workspaceCleanup.groovy
+++
@@ -1,6 +0,0 @@
-def call() {
-    def ws = currentBuild.getWorkspace()
-    ws.deleteRecursive()
-    ws.mkdirs()
-    echo "Workspace cleaned up."
-}
